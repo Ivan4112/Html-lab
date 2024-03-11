a7149709dc7a1c599bef3023a80e423e6cbc0996
Ivan4112 <ivanbondar224@gmail.com>
2024-03-11 22:55:38 +0200

    Initial commit

---
+++ src/main/java/fpm/dnu/org/htmllab/HtmlLabApplication.java
@@ -0,0 +1,13 @@
+package fpm.dnu.org.htmllab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtmlLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fpm/dnu/org/htmllab/HtmlLabApplicationTests.java
@@ -0,0 +1,13 @@
+package fpm.dnu.org.htmllab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
